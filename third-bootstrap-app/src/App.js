2ccc5a65ccb7e8a0aa38a322a634b4882d97bc8e
Shivam verma <shivamverma@Shivams-MacBook-Air-2.local>
2024-01-27 14:05:29 +0530

    Third App completed

--- third-bootstrap-app/src/App.js
+++ third-bootstrap-app/src/App.js
@@ -1,15 +1,42 @@
+import { useState } from 'react';
 import './App.css';
+// import About from './modules/About';
 import Navbar from './modules/Navbar';
 import TextArea from './modules/TextArea';
+import Alert from './modules/Alert';
 
 function App() {
+  const [currentMode, newMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert]=useState(null);
+  
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+  const toggleMode=()=>{
+    if(currentMode=='light'){
+      newMode('dark');
+      document.body.style.backgroundColor='grey';
+      showAlert("Dark Mode has been enabled","success");
+    }else{
+      newMode('light')
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode has been enabled","success");
+    }
+  }
   return (
     <>
-    <Navbar title="Textutils" aboutText="About us"/>
+    <Navbar title="Textutils" aboutText="About us" mode={currentMode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     {/* <Navbar/> */}
     <div className="container">
-      
-        <TextArea title="Enter your details here"/>
+        <TextArea showAlert={showAlert} title="Enter your details here" mode={currentMode}/>
+        {/* <About></About> */}
     </div>
     </>
   );
--- third-bootstrap-app/src/modules/TextArea.js
+++ third-bootstrap-app/src/modules/TextArea.js
@@ -5,11 +5,13 @@ export default function TextArea(props) {
         console.log("Uppercase was Clicked"+text);
         let newText=text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted UpperCase Successfully","success");
     }
     const handleLowClick=()=>{
         console.log("Lowercase was Clicked"+text);
         let newText=text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted LowerCase Successfully","success");
     }
     const handleClearClick=()=>{
         console.log("Clear text");
@@ -22,16 +24,16 @@ export default function TextArea(props) {
     const [text,setText]= useState("Enter text here");
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode === 'light'?'black':'white'}}>
             <h1>{props.title}</h1>
             <div className="mb-3">
-                <textarea value={text} onChange={handleOnChange} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+                <textarea value={text} onChange={handleOnChange} className="form-control" style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'light'?'black':'white'}} id="exampleFormControlTextarea1" rows="8"></textarea>
             </div>
             <button onClick={handleUpClick} className="btn btn-primary mx-1">UpperCase</button>
             <button onClick={handleLowClick} className="btn btn-primary mx-1">LowerCase</button>
             <button onClick={handleClearClick} className="btn btn-primary mx-1">Clear Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'light'?'black':'white'}}>
             <h1>Your text summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 *text.split(" ").length} Minutes read</p>
