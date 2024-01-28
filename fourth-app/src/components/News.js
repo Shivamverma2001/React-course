66afbeb0a735b02d7072b53d1bc73b55abb48dc4
Shivam verma <shivamverma@Shivams-MacBook-Air-2.local>
2024-01-28 16:39:34 +0530

    Spinner

--- fourth-app/src/components/News.js
+++ fourth-app/src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 
 // dab0f9b4b8574e2b9ad482360fe4f7c8
@@ -11,7 +12,7 @@ export class News extends Component {
   //       "name": "CBS Sports"
   //     },
   //     "author": "Shakiel Mahjouri",
-  //     "title": "2024 WWE Royal Rumble results, recap, grades: Cody Rhodes eliminates CM Punk last, Bayley wins first Rumble - CBS Sports",
+  //     "title": "{24 WWE Royal Rumble results, recap, grades: Cody Rhodes eliminates CM Punk last, Bayley wins first Rumble - CBS Sports",
   //     "description": "Rhodes becomes the first superstar in 26 years to win consecutive Royal Rumble matches",
   //     "url": "https://www.cbssports.com/wwe/news/2024-wwe-royal-rumble-results-recap-grades-cody-rhodes-eliminates-cm-punk-last-bayley-wins-first-rumble/live/",
   //     "urlToImage": "https://sportshub.cbsistatic.com/i/r/2024/01/28/d169d28a-7276-415f-b8f5-b2df47fe8605/thumbnail/1200x675/56ff001d125cd8e248e66eeb50dfb4c4/cody-rhodes-rumble-winner.jpg",
@@ -277,43 +278,57 @@ export class News extends Component {
     }
   }
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=dab0f9b4b8574e2b9ad482360fe4f7c8&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dab0f9b4b8574e2b9ad482360fe4f7c8&page=1&pageSize=${this.props.pageSize}`;
 
+    this.setState({
+      loading:true
+    })
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
-      parseResult: parsedData.totalResults
+      parseResult: parsedData.totalResults,
+      loading:false
     })
   }
   handleNextClick = async () => {
-    if(this.state.page+1> Math.ceil(this.state.totalResults/20)){
-      
-    }else{
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dab0f9b4b8574e2b9ad482360fe4f7c8&page=${this.state.page + 1}&pageSize=20`;
+    if(!(this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize))){
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dab0f9b4b8574e2b9ad482360fe4f7c8&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({
+        loading: true
+      })
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         articles: parsedData.articles,
-        page: this.state.page + 1
+        page: this.state.page + 1,
+        loading:false
       }) 
     }
   }
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dab0f9b4b8574e2b9ad482360fe4f7c8&page=${this.state.page - 1}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dab0f9b4b8574e2b9ad482360fe4f7c8&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading:true
+    })
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
+      
+    })
+    this.setState({
       articles: parsedData.articles,
-      page: this.state.page11
+      page: this.state.page-1,
+      loading:false
     })
   }
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className='text-center'>NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             if (element.title != null && element.description != null && element.urlToImage != null) {
               return <div className="col-md-4" key={element.url}>
                 <NewsItem myTitle={element.title.slice(0, 45)} myDesc={element.description.slice(0, 88)} imageUrl={element.urlToImage} newsUrl={element.url} />
@@ -323,7 +338,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
           <button disabled={this.state.page <= 1} type="button" className='btn btn-dark' onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button type="button" className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
