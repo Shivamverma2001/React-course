ec2b33c5b861ec1b4124e050a227f3d00daa44d4
Shivam verma <shivamverma@Shivams-MacBook-Air-2.local>
2024-01-29 14:21:27 +0530

    Adding top loading bar

--- fourth-app/src/components/NewsItem.js
+++ fourth-app/src/components/NewsItem.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { myTitle, myDesc, imageUrl, newsUrl, author, date,source } = this.props;
+    let { myTitle, myDesc, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className='my-3'>
         <div className="card">
-        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
-                {source}
-                <span class="visually-hidden">unread messages</span>
-              </span>
+          <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+            <span className="badge rounded-pill bg-danger">
+              {source}
+              <span className="visually-hidden">unread messages</span>
+            </span>
+          </div>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">
